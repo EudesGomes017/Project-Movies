30ac9a12c1e087f797ed963e723c08b068735b28
EudesGomes017 <eudesgomes017@gmail.com>
2024-04-16 12:08:20 -0300

    Create pagination

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,6 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import Listing from 'Page/Listing';
-import Form from 'Page/Form';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listing from "Page/Listing";
+import Form from "Page/Form";
 import Navbar from "components/Navbar";
 
 function App() {
