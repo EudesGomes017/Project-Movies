92c3e9479ce2cb5467be73218d3de9e74849b126
EudesGomes017 <eudesgomes017@gmail.com>
2024-04-24 16:01:06 -0300

    erro

---
+++ backend/src/main/java/com/Dleawebsystem/pjmovie/entities/Score.java
@@ -0,0 +1,43 @@
+package com.Dleawebsystem.pjmovie.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+
+	private Double avalicao;
+
+	public Score() {
+
+	}
+
+	public Score(Movie movie, User user, Double avalicao) {
+
+		id.setMovie(movie);
+		id.setUser(user);
+		this.avalicao = avalicao;
+	}
+
+	public Double getAvalicao() {
+		return avalicao;
+	}
+
+	public void setAvalicao(Double avalicao) {
+		this.avalicao = avalicao;
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+}
