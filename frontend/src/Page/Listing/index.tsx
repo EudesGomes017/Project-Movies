cdf5ec61292dcae23aa7524be34861c6447b6402
EudesGomes017 <eudesgomes017@gmail.com>
2024-05-04 09:29:26 -0300

    insert movie score

--- frontend/src/Page/Listing/index.tsx
+++ frontend/src/Page/Listing/index.tsx
@@ -1,16 +1,47 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/request";
 
 function Listing() {
+  // o react ele tem circulo de requsições com isso trabalhando com hooks podemos  vincular com o circulo do react !
 
-  //fomra errada
+  /*Hook: useState
+Manter estado no componente */
 
-  axios.get(`${BASE_URL}/movies?size=12&page=0`)
-  .then(response => {
-    console.log(response.data)
-  })
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
+  });
+
+  /*Hook: useEffect
+Executar algo na instanciação ou destruição do componente, observar estado */
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+      .then((response) => {
+        const data = response.data as MoviePage;
+        setPage(data);
+        console.log(data)
+      });
+    
+  }, [pageNumber]);
+
+  /*fomra errada
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+    console.log(response.data);*/
 
   return (
     <>
@@ -18,24 +49,13 @@ function Listing() {
 
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
+          {page.content.map((movie) => {
+            return (
+              <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                <MovieCard movie={movie} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
